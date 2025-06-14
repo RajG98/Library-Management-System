be75e97ca0df2d2d99699ee46fa1aa5a82db0ebe
RajG98 <rajarshig007@gmail.com>
2025-06-15 00:24:07 +0530

    add

--- backend/src/main/java/com/project/DigitalLibraryManagement/config/SecurityConfig.java
+++ backend/src/main/java/com/project/DigitalLibraryManagement/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Specify the allowed origin
+        configuration.setAllowedOrigins(List.of("https://library-management-system-seven-mu.vercel.app")); // Specify the allowed origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Allow credentials
